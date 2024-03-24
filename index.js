5500be149d1cb59a70735105dfe3cd4fddb67a96
RahulBis <rahulsinghbisht125@gmail.com>
2024-03-24 10:28:01 +0530

    commit in index.js commenting CORS

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ const port = process.env.PORT || 5000;
 const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017';
 
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173'
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:5173'
+// }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 
 
 app.use(cookieParser());
