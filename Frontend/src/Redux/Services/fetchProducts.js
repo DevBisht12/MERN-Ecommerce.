bc373698b9ca107b72f4cab931d8c129516a7f54
RahulBis <rahulsinghbisht125@gmail.com>
2024-03-24 14:22:59 +0530

    commit in reux fetchProducts.js

--- Frontend/src/Redux/Services/fetchProducts.js
+++ Frontend/src/Redux/Services/fetchProducts.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const productsApi = createApi({
   reducerPath:'productsApi',
-  baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/api/'}),
+  baseQuery: fetchBaseQuery({baseUrl: 'https://mern-ecommerce-4ewf.onrender.com/'}),
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: ({ page, categoryName, sort }) => {
